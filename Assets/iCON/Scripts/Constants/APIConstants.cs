966020a1dff30a620cf1fa0e9e951b1c9455cc76
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-29 00:43:26 +0900

    add:APIリクエスト関連の定数を追加

--- Assets/iCON/Scripts/Constants/APIConstants.cs
+++ Assets/iCON/Scripts/Constants/APIConstants.cs
@@ -9,5 +9,10 @@ namespace iCON.Constants
         /// サービスアカウントキーのファイル名
         /// </summary>
         public const string SERVICE_ACCOUNT_KEY_FILE_NAME = "service-account-key.json";
+
+        /// <summary>
+        /// APIリクエスト時に送信されるアプリケーション名。Google側でのログや制限管理に使用される
+        /// </summary>
+        public const string APPLICATION_NAME = "iCON-MasterDataLoader";
     }
 }
