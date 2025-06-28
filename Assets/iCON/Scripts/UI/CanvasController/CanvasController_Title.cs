989cc17c665ccebfd6713e1c4435fc3f426b9243
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-28 19:56:57 +0900

    feat:TitleのCanvasControllerを追加

---
+++ Assets/iCON/Scripts/UI/CanvasController/CanvasController_Title.cs
@@ -0,0 +1,35 @@
+using System;
+using Cysharp.Threading.Tasks;
+using iCON.System;
+using UnityEngine;
+
+namespace iCON.UI
+{
+    /// <summary>
+    /// CanvasController_Title
+    /// </summary>
+    public partial class CanvasController_Title : WindowBase
+    {
+        [SerializeField, HighlightIfNull] private CustomButton _startButton;
+        
+        public event Action OnStartButtonClicked;
+        
+        public override UniTask OnAwake()
+        {
+            // イベント登録
+            if(_startButton != null) _startButton.onClick.AddListener(HandleStartButtonClicked);
+            return base.OnAwake();
+        }
+
+        private void HandleStartButtonClicked()
+        {
+            SceneLoader.Instance.LoadScene(3);
+            OnStartButtonClicked?.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            if(_startButton != null) _startButton.onClick?.RemoveAllListeners();
+        }
+    }
+}
