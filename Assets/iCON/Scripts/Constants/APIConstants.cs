230cf8e155f7c1ba6de74ac81338543bfe1d53e0
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-29 00:14:20 +0900

    feat:通信関連の定数まとめクラスを作成

---
+++ Assets/iCON/Scripts/Constants/APIConstants.cs
@@ -0,0 +1,13 @@
+namespace iCON.Constants
+{
+    /// <summary>
+    /// 通信に関連する定数
+    /// </summary>
+    public static class APIConstants
+    {
+        /// <summary>
+        /// サービスアカウントキーのファイル名
+        /// </summary>
+        public const string SERVICE_ACCOUNT_KEY_FILE_NAME = "service-account-key.json";
+    }
+}
