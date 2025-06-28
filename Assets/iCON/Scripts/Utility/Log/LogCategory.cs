86d3d802fe9ff80b1103bd8fbb6b852dfae2c6e0
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-28 13:39:30 +0900

    feat:ログカテゴリの列挙型を追加

---
+++ Assets/iCON/Scripts/Utility/Log/LogCategory.cs
@@ -0,0 +1,18 @@
+namespace iCON.Utility
+{
+    /// <summary>
+    /// ログカテゴリ定義
+    /// </summary>
+    public enum LogCategory
+    {
+        General,
+        System,
+        Gameplay,
+        UI,
+        Audio,
+        Network,
+        Performance,
+        Test,
+        Debug
+    }
+}
