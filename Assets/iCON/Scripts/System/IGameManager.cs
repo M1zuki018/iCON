6f173a8d8f18fc3caee0a9079ba3278206903719
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-28 15:54:26 +0900

    feat:GameManager関連のクラスをインポート

---
+++ Assets/iCON/Scripts/System/IGameManager.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// 依存性を低減させるためのGameManager用のインターフェース
+/// </summary>
+public interface IGameManager
+{
+    // GameSettings Settings { get; }
+    // bool IsFirstLoad { get; }
+    // int VictoryPoints { get; }
+    // BattleModeData GetGameModeData();
+    // void SetGameMode(GameModeEnum mode);
+    // void SetGameState(GameStateEnum gameState);
+    // int SetVictoryPoints(int points);
+}
