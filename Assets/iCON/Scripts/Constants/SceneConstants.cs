76a20163e7653cc5118b42b6cc2c5417d1526647
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-28 11:56:15 +0900

    feat:シーン関連の定数を宣言するクラスを追加

---
+++ Assets/iCON/Scripts/Constants/SceneConstants.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace iCON.Constants
+{
+    /// <summary>
+    /// シーンに関連する定数
+    /// </summary>
+    public static class SceneConstants
+    {
+        /// <summary>
+        /// Systemシーンの個数
+        /// </summary>
+        public const int SYSTEM_SCENE_COUNT = 2; 
+    }
+
+}
