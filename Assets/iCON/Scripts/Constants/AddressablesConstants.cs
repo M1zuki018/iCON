1dc562a834f0473751a2e81726de8c87a3ca84c8
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-29 20:16:58 +0900

    feat:Addressable利用のための定数を作成（試作）

---
+++ Assets/iCON/Scripts/Constants/AddressablesConstants.cs
@@ -0,0 +1,18 @@
+namespace iCON.Constants
+{
+    /// <summary>
+    /// Addressableの画像呼び出しに関連する定数
+    /// </summary>
+    public static class AddressableConstants
+    {
+        /// <summary>
+        /// キャラクター
+        /// </summary>
+        public const string CHARACTER_PATH = "Assets/AssetStoreTools/Image/Character/";
+        
+        /// <summary>
+        /// スチル
+        /// </summary>
+        public const string STEEL_PATH = "Assets/AssetStoreTools/Images/Steel/";
+    }
+}
