2db660b1a752e7f97205738b85ad1ef916e4502f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-29 20:29:04 +0900

    feat:ストーリーイベントの列挙型を追加

---
+++ Assets/iCON/Enum/Story/StoryEventType.cs
@@ -0,0 +1,23 @@
+namespace iCON.Enums
+{
+    /// <summary>
+    /// ストーリーイベントの列挙型
+    /// </summary>
+    public enum StoryEventType 
+    {
+        Start = 0,
+        Talk = 1,
+        Descriptive = 2,
+        End = 3,
+        ChangeBGM = 4,
+        InCharacter = 5,
+        OutCharacter = 6,
+        ShowSteel = 7,
+        HideSteel = 8,
+        CameraShake = 9,
+        Choice = 10,
+        Effect = 11,
+        ChangeBackground = 12,
+    }
+}
+
