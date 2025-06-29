a9a68d98614d952a0385397a583152b678be7e27
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-06-29 15:48:55 +0900

    feat:キャラクターの立ち位置の列挙型を追加

---
+++ Assets/iCON/Scripts/UI/CanvasController/CanvasController_Story/StoryCharacterPositionType.cs
@@ -0,0 +1,23 @@
+namespace iCON.UI
+{
+    /// <summary>
+    /// ストーリー中のキャラクターの立ち位置の列挙型
+    /// </summary>
+    public enum StoryCharacterPositionType
+    {
+        /// <summary>
+        /// 中央
+        /// </summary>
+        Center = 0,
+        
+        /// <summary>
+        /// 左
+        /// </summary>
+        Left = 1,
+        
+        /// <summary>
+        /// 右
+        /// </summary>
+        Right = 2,
+    }
+}
