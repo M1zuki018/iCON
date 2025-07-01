d66e1cb0926db81be21d666c97bffb16d2db61fe
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-01 14:59:53 +0900

    feat:オーダーの実行クラスを作成

---
+++ Assets/iCON/Scripts/System/Story/Executor/OrderExecutor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace iCON.System
+{
+    /// <summary>
+    /// ストーリーのオーダーを実行するクラス
+    /// </summary>
+    public class OrderExecutor
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
