e7dcdf8edec6e6bd939538740531502c83443bc5
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-02 00:04:45 +0900

    feat:ストーリーに関する定数クラスを追加

---
+++ Assets/iCON/Scripts/Constants/StoryConstants.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+
+namespace iCON.Constants
+{
+    /// <summary>
+    /// ストーリーに関連する定数
+    /// </summary>
+    public static class StoryConstants
+    {
+        /// <summary>
+        /// フェード時間
+        /// </summary>
+        public const float IMAGE_FADE_DURATION = 0.3f;
+
+        /// <summary>
+        /// フェード時のイージング
+        /// </summary>
+        public const Ease FADE_EASE = Ease.Linear;
+    }
+}
