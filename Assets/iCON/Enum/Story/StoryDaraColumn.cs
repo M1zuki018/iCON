d7e8976e613b91da904cec9afcf3d5309ff125ae
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-01 14:34:41 +0900

    feat:Spreadseatの列項目の列挙型（列の変更によるデータのIndexの変化に強くしたい）

---
+++ Assets/iCON/Enum/Story/StoryDaraColumn.cs
@@ -0,0 +1,23 @@
+namespace iCON.Enums
+{
+    /// <summary>
+    /// スプレッドシートの列定義
+    /// </summary>
+    public enum StoryDataColumn
+    {
+        PartId = 0,
+        ChapterId = 1,
+        SceneId = 2,
+        OrderId = 3,
+        OrderType = 4,
+        Sequence = 5,
+        SpeakerId = 6,
+        DialogText = 7,
+        OverrideDisplayName = 8,
+        FilePath = 9,
+        Position = 10,
+        FacialExpressionType = 11,
+        OverrideTextSpeed = 12,
+        Duration = 13
+    }
+}
