16d97fd2eb4ce2ef78bb6cd4a734a19f815dc070
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-10 21:21:59 +0900

    add:InputActionの定数定義

---
+++ Assets/iCON/Scripts/Constants/InputActionConstants.cs
@@ -0,0 +1,15 @@
+namespace iCON.Constants
+{
+    /// <summary>
+    /// InputActionの定数定義
+    /// </summary>
+    public static class InputActionConstants
+    {
+        public const string MOVE = "Move";
+        public const string DASH = "Dash";
+        public const string SHORTCUT = "Shortcut";
+        public const string PAUSE = "Pause";
+        public const string CHARAMENU = "CharaMenu";
+        public const string CONFIRM = "Confirm";
+    }
+}
