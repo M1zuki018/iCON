346eb210a6c9c6770905a7150dcdbe42eb247161
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-10 23:13:38 +0900

    feat:タイトルシーンのマネージャーを追加

---
+++ Assets/iCON/Scripts/System/TitleSceneManager.cs
@@ -0,0 +1,46 @@
+using Cysharp.Threading.Tasks;
+using iCON.Performance;
+using iCON.Utility;
+using UnityEngine;
+
+namespace iCON.System
+{
+    /// <summary>
+    /// タイトルシーンのマネージャー
+    /// </summary>
+    public class TitleSceneManager : ViewBase
+    {
+        /// <summary>
+        /// タイトルスプラッシュの演出マネージャー
+        /// </summary>
+        [SerializeField, HighlightIfNull]
+        private TitleSplashManager _titleSplashManager;
+        
+        /// <summary>
+        /// Start
+        /// </summary>
+        public override UniTask OnStart()
+        {
+            if (_titleSplashManager == null)
+            {
+                LogUtility.Error("TitleSplashManagerがアサインされていません", LogCategory.System);
+                return base.OnStart();
+            }
+            
+            // タイトルスプラッシュの演出を開始
+            _titleSplashManager.Play();
+            return base.OnStart();
+        }
+        
+        /// <summary>
+        /// Update
+        /// </summary>
+        private void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+            {
+                _titleSplashManager.Skip();
+            }
+        }
+    }
+}
