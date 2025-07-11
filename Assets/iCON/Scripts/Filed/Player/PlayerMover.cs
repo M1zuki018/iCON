8c644f5ca6092f46733afdfb4e6bdc41dafc1459
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-11 23:23:57 +0900

    feat:仮でプレイヤーの四方向の移動を作成（入力系を直したら本作成）

--- Assets/iCON/Scripts/Filed/Player/PlayerMover.cs
+++ Assets/iCON/Scripts/Filed/Player/PlayerMover.cs
@@ -1,5 +1,6 @@
 using iCON.Enums;
 using iCON.UI;
+using iCON.Utility;
 using UnityEngine;
 
 namespace iCON.Field.Player
@@ -9,8 +10,17 @@ namespace iCON.Field.Player
     /// </summary>
     public class PlayerMover : MonoBehaviour
     {
+        /// <summary>
+        /// スプライトアニメーションのコントローラー
+        /// </summary>
         [SerializeField]
         private SpriteAnimationController _animController;
+
+        /// <summary>
+        /// 移動速度
+        /// </summary>
+        [SerializeField] 
+        private float _moveSpeed = 5f;
 
         /// <summary>
         /// 現在向いている方向
@@ -19,25 +29,31 @@ namespace iCON.Field.Player
         
         private void Update()
         {
-            if (UnityEngine.Input.GetKeyDown(KeyCode.A))
+            if (UnityEngine.Input.GetKey(KeyCode.A))
             {
                 _directionType = MoveDirectionType.Left;
             }
-
-            if (UnityEngine.Input.GetKeyDown(KeyCode.D))
+            else if (UnityEngine.Input.GetKey(KeyCode.D))
             {
                 _directionType = MoveDirectionType.Right;
             }
-
-            if (UnityEngine.Input.GetKeyDown(KeyCode.W))
+            else if (UnityEngine.Input.GetKey(KeyCode.W))
             {
                 _directionType = MoveDirectionType.Up;
             }
-
-            if (UnityEngine.Input.GetKeyDown(KeyCode.S))
+            else if (UnityEngine.Input.GetKey(KeyCode.S))
             {
                 _directionType = MoveDirectionType.Down;
             }
+            else
+            {
+                _directionType = MoveDirectionType.None;
+            }
+            
+            var direction = DirectionUtility.GetVector2(_directionType);
+            // Time.deltaTimeと移動速度を掛けて適切な移動量にする
+            Vector3 movement = new Vector3(direction.x, direction.y, 0) * _moveSpeed * Time.deltaTime;
+            transform.position += movement;
         }
     }
 }
