3f9e99125f19c86af0afb26cbe946a695cab1bc8
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-12 13:33:30 +0900

    feat:プレイヤーのスプライトアニメーションの画像を設定するスクリプタブルオブジェクトを作成

---
+++ Assets/iCON/Scripts/Filed/Player/PlayerAnimationSetting.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using iCON.Enums;
+using UnityEngine;
+
+namespace iCON.Field.Player
+{
+    /// <summary>
+    /// プレイヤーの歩行アニメーションのSpriteを設定するスクリプタブルオブジェクト
+    /// </summary>
+    [CreateAssetMenu(fileName = "PlayerAnimationSetting", menuName = "Scriptable Objects/Player Animation Setting")]
+    public class PlayerAnimationSetting : ScriptableObject
+    {
+        [SerializeField] private List<Sprite> _leftSprites = new List<Sprite>();
+        [SerializeField] private List<Sprite> _rightSprites = new List<Sprite>();
+        [SerializeField] private List<Sprite> _upSprites = new List<Sprite>();
+        [SerializeField] private List<Sprite> _downSprites = new List<Sprite>();
+
+        /// <summary>
+        /// 引数で指定した方向に対応した画像リストを取得する
+        /// </summary>
+        public List<Sprite> GetSprites(MoveDirectionType direction)
+        {
+            var sprites = direction switch
+            {
+                MoveDirectionType.Left => _leftSprites,
+                MoveDirectionType.Right => _rightSprites,
+                MoveDirectionType.Up => _upSprites,
+                MoveDirectionType.Down => _downSprites,
+                _ => null,
+            };
+            
+            return sprites;
+        }
+    }
+
+}
