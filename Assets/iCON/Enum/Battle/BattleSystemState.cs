5cbdc2035758e13889695157e2fd254af084497f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-13 00:50:07 +0900

    feat:バトルの進行状態の列挙型を追加

---
+++ Assets/iCON/Enum/Battle/BattleSystemState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace iCON.Enums
+{
+    /// <summary>
+    /// バトルの進行状態の列挙型
+    /// </summary>
+    public enum BattleSystemState
+    {
+        None,
+        FirstSelect,
+        TryEscape,
+        ActionSelect,
+    }
+
+}
