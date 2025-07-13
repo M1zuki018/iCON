845a37f2f5c717def450393ec7fee540660306ab
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-13 15:22:41 +0900

    feat:文言キーとメッセージのペアのクラスを追加

---
+++ Assets/iCON/Scripts/Generated/WordingMaster.cs
@@ -0,0 +1,22 @@
+// ============================================================================
+// AUTO GENERATED - DO NOT MODIFY
+// Generated at: 2025-07-13 15:05:19
+// ============================================================================
+
+using System.Collections.Generic;
+
+/// <summary>
+/// ワーディングキー定数クラス
+/// </summary>
+public static class WordingMaster
+{
+    private static readonly Dictionary<string, string> _data = new Dictionary<string, string>
+    {
+        { "BOOT_START", "Start" },
+    };
+
+    public static string GetText(string key)
+    {
+        return _data.GetValueOrDefault(key, key);
+    }
+}
