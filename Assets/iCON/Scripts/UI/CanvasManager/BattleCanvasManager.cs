d07a1d54814eabc3909e08557631ffd140f7a813
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-13 13:03:21 +0900

    add:CanvasManagerに現在表示中のキャンバスを取得する処理を追加

--- Assets/iCON/Scripts/UI/CanvasManager/BattleCanvasManager.cs
+++ Assets/iCON/Scripts/UI/CanvasManager/BattleCanvasManager.cs
@@ -7,6 +7,14 @@ namespace iCON.UI
     /// </summary>
     public class BattleCanvasManager : SceneCanvasManagerBase
     {
+        /// <summary>
+        /// 現在表示中のウィンドウを取得する
+        /// </summary>
+        public WindowBase CurrentCanvas => _canvasObjects[_currentCanvasIndex];
+        
+        /// <summary>
+        /// キャンバスを切り替える
+        /// </summary>
         public void ShowCanvas(BattleCanvasType canvasType)
         {
             base.ShowCanvas((int)canvasType);
