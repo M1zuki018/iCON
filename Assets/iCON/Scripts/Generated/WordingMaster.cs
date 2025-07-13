102c4d739124a9f00938e936e9de9d5e6b469c7f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-13 16:07:16 +0900

    update:Wording更新

--- Assets/iCON/Scripts/Generated/WordingMaster.cs
+++ Assets/iCON/Scripts/Generated/WordingMaster.cs
@@ -1,6 +1,6 @@
 // ============================================================================
 // AUTO GENERATED - DO NOT MODIFY
-// Generated at: 2025-07-13 15:05:19
+// Generated at: 2025-07-13 16:04:45
 // ============================================================================
 
 using System.Collections.Generic;
@@ -13,6 +13,16 @@ public static class WordingMaster
     private static readonly Dictionary<string, string> _data = new Dictionary<string, string>
     {
         { "BOOT_START", "Start" },
+        { "LOADING", "Loading..." },
+        { "STORY_IMMERSED", "UI\n非表示" },
+        { "STORY_AUTO", "オート\n再生" },
+        { "STORY_SKIP", "スキップ" },
+        { "BATTLE_COMMAND_ATTCK", "アタック" },
+        { "BATTLE_COMMAND_IDEA", "idea" },
+        { "BATTLE_COMMAND_ITEM", "item" },
+        { "BATTLE_COMMAND_GUARD", "まもる" },
+        { "BATTLE_FIRSTSELECT_START", "たたかう" },
+        { "BATTLE_FIRSTSELECT_ESCAPE", "にげる" },
     };
 
     public static string GetText(string key)
