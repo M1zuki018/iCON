6a1dfa7a3eaeb7f1666f9a2140f4b43ae75fe106
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-15 07:52:29 +0900

    update:プレイヤーがダメージを受けた時にダメージ量の表記を行うイベントを追加

--- Assets/iCON/Scripts/UI/CanvasManager/BattleCanvasManager.cs
+++ Assets/iCON/Scripts/UI/CanvasManager/BattleCanvasManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using iCON.Battle;
 using iCON.Enums;
 using UnityEngine;
@@ -17,6 +18,12 @@ namespace iCON.UI
         [SerializeField]
         private CharacterIconContents _iconContents;
         private List<CharacterIconContents> _icons;
+        
+        /// <summary>
+        /// ダメージテキストのオブジェクトプールを管理するクラス
+        /// </summary>
+        [SerializeField]
+        private DamageTextPool _damageTextPool;
         
         /// <summary>
         /// 現在表示中のウィンドウを取得する
@@ -44,6 +51,7 @@ namespace iCON.UI
                 // 自身の子オブジェクトとしてアイコンを生成
                 var icon = Instantiate(_iconContents, transform);
                 _icons.Add(icon);
+                icon.Setup(_damageTextPool);
             }
             
             SubscribeToUnitEvents(unitData);
@@ -60,18 +68,22 @@ namespace iCON.UI
                 var unit = unitData[i];
                 var index = i; // ローカル変数でキャプチャ
                 
-                unit.OnHpChanged += (currentHp, maxHp) => UpdatePlayerIcon(index, currentHp, maxHp);
-                //unit.OnSpChanged += (newSp) => UpdatePlayerIcon(index);
-                //unit.OnDeath += () => UpdatePlayerIcon(index);
+                unit.OnHpChanged += (currentHp, maxHp, damage) => UpdatePlayer(index, currentHp, maxHp, damage);
+                //unit.OnSpChanged += (newSp) => UpdatePlayer(index);
+                //unit.OnDeath += () => UpdatePlayer(index);
             }
         }
 
         /// <summary>
-        /// プレイヤーのHPバーを更新
+        /// ダメージを受けた時のUI更新
         /// </summary>
-        private void UpdatePlayerIcon(int index, int value, int maxValue)
+        private void UpdatePlayer(int index, int value, int maxValue, int damage)
         {
+            // HPバーを更新
             _icons[index].SetHpSlider(value, maxValue);
+            
+            // ダメージ量の表記を行う
+            _icons[index].SetDamageText(damage).Forget();
         }
     }
 
