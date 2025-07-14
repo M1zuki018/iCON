1dee27aac04453ff90a1706dd6b72899de59e0a3
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-14 10:28:10 +0900

    feat:コマンドの種類の列挙型を追加

---
+++ Assets/iCON/Enum/Battle/CommandType.cs
@@ -0,0 +1,29 @@
+namespace iCON.Enums
+{
+    /// <summary>
+    /// コマンドの種類の列挙型
+    /// </summary>
+    public enum CommandType
+    {
+        /// <summary>
+        /// 攻撃
+        /// </summary>
+        Attack,
+        
+        /// <summary>
+        /// ガード
+        /// </summary>
+        Guard,
+        
+        /// <summary>
+        /// アイデア（スキル）
+        /// </summary>
+        Idea,
+        
+        /// <summary>
+        /// アイテム
+        /// </summary>
+        Item,
+    }
+
+}
