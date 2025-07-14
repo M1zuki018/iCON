7ff41377769ccbfa8d9dd3b9e1ed0fe6c2e52b0a
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-14 12:12:48 +0900

    fix:BattleManager内にあった終了条件チェックをExecuteState側で見るように変更

--- Assets/iCON/Scripts/System/Battle/StateMachine/ExecuteState.cs
+++ Assets/iCON/Scripts/System/Battle/StateMachine/ExecuteState.cs
@@ -13,9 +13,19 @@ namespace iCON.Battle
             base.Enter(manager, view);
             view.ShowCanvas(BattleCanvasType.Execute);
 
-            await manager.ExecuteBattle();
-            
-            manager.SetState(BattleSystemState.FirstSelect);
+            // バトル実行を待つ
+            var result = await manager.ExecuteBattle();
+
+            if (result.isFinish)
+            {
+                // バトルが終了している場合は勝敗に合わせてステートを変更
+                manager.SetState(result.isWin ? BattleSystemState.Win : BattleSystemState.Lose);
+            }
+            else
+            {
+                // バトルが終了していなければ最初の選択に戻る
+                manager.SetState(BattleSystemState.FirstSelect);
+            }
         }
     }
 
