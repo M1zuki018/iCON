0028cdf7fba8ef9333110ce51725dfa0ee9826aa
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-14 10:39:50 +0900

    feat:バトルコマンドはFactoryパターンで作成するので、Factoryクラスを追加

---
+++ Assets/iCON/Scripts/System/Battle/Command/CommandFactory/BattleCommandFactory.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using iCON.Enums;
+
+namespace iCON.Battle
+{
+    /// <summary>
+    /// バトルコマンドオブジェクトを生成・管理するファクトリークラス
+    /// </summary>
+    public static class BattleCommandFactory
+    {
+        /// <summary>
+        /// 生成済みコマンドインスタンスのキャッシュ
+        /// </summary>
+        private static readonly Dictionary<CommandType, IBattleCommand> _commandCache = new();
+        
+        /// <summary>
+        /// 指定されたコマンドタイプに対応するコマンドオブジェクトを取得する
+        /// サポートされていないコマンドを要求した場合nullを返す
+        /// </summary>
+        [return: MaybeNull]
+        public static IBattleCommand GetCommand(CommandType commandType)
+        {
+            if (_commandCache.TryGetValue(commandType, out var cachedCommand))
+            {
+                // 既に生成済みのコマンドがあればそれを再利用する
+                return cachedCommand;
+            }
+            
+            // キャッシュに存在しない場合は、引数に対応するコマンドを新規生成
+            IBattleCommand command = commandType switch
+            {
+                CommandType.Attack => new AttackCommand(), 
+                CommandType.Guard => new GuardCommand(),
+                // CommandType.Idea => new IdeaCommand(),
+                // CommandType.Item => new ItemCommand(),
+                _ => null
+            };
+            
+            if (command != null)
+            {
+                // 生成に成功した場合はキャッシュに保存
+                _commandCache[commandType] = command;
+            }
+            
+            return command;
+        }
+    }
+}
