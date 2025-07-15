88145f8b3b2ee45285d533f7156a95997f9e8b0b
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-15 22:58:16 +0900

    feat:アイデアの効果対象の指定方法の列挙型

---
+++ Assets/iCON/Enum/Battle/TargetSelectionType.cs
@@ -0,0 +1,19 @@
+namespace iCON.Enums
+{
+    /// <summary>
+    /// 効果対象の指定方法
+    /// </summary>
+    public enum TargetSelectionType
+    {
+        /// <summary>
+        /// ランダム
+        /// </summary>
+        Random,
+        
+        /// <summary>
+        /// 選択
+        /// </summary>
+        Select,
+    }
+}
+
