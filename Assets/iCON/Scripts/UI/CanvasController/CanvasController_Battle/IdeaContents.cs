924cea65af748da798657563983ee28a4a5dd076
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-15 09:10:44 +0900

    feat:Idea選択ボタンを管理するクラスを実装

---
+++ Assets/iCON/Scripts/UI/CanvasController/CanvasController_Battle/IdeaContents.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace iCON.Battle
+{
+    /// <summary>
+    /// Idea（スキル）のコンテンツ
+    /// </summary>
+    public class IdeaContents : MonoBehaviour
+    {
+        /// <summary>
+        /// ボタンリスト
+        /// </summary>
+        [SerializeField]
+        private List<Button> _ideaButtons;
+        
+        /// <summary>
+        /// CanvasGroup
+        /// </summary>
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+        
+        /// <summary>
+        /// アイデア選択用のボタンリスト
+        /// </summary>
+        public List<Button> IdeaButtons => _ideaButtons;
+        
+        /// <summary>
+        /// CanvasGroup
+        /// </summary>
+        public CanvasGroup CanvasGroup => _canvasGroup;
+    }
+}
