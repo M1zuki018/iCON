0a2c26ca0d5502999356ca9e0025fb91f4b1b94e
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-07-24 13:03:54 +0900

    update:表情差分の列挙型の種類を追加

--- Assets/iCON/Enum/Story/FacialExpressionType.cs
+++ Assets/iCON/Enum/Story/FacialExpressionType.cs
@@ -5,6 +5,25 @@ namespace iCON.System
     /// </summary>
     public enum FacialExpressionType
     {
+        /// <summary>デフォルト</summary>
         Default,
+        
+        /// <summary>緊張</summary>
+        Nervous,
+        
+        /// <summary>溜息</summary>
+        Sigh,
+        
+        /// <summary>驚き</summary>
+        Surprised,
+        
+        /// <summary>微笑み</summary>
+        Smile,
+        
+        /// <summary>照れ</summary>
+        Blush,
+        
+        /// <summary>恥ずかしい</summary>
+        Embarrassed,
     }    
 }
