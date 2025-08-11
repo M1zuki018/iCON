53e7e2edcfa91bd89bae0365882307ad566e2297
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-11 11:08:27 +0900

    add:ボタンの参照を追加

--- Assets/_iCON/Runtime/Scripts/UI/CanvasController/CanvasController_Title/CanvasController_Title.cs
+++ Assets/_iCON/Runtime/Scripts/UI/CanvasController/CanvasController_Title/CanvasController_Title.cs
@@ -3,6 +3,7 @@ using CryStar.Attribute;
 using CryStar.Utility;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace iCON.UI
 {
@@ -12,46 +13,102 @@ namespace iCON.UI
     public partial class CanvasController_Title : WindowBase
     {
         /// <summary>
-        ///  スタートボタンを押した時のコールバック
+        /// ゲームを最初から始めるボタンを押したときのコールバック
         /// </summary>
-        public event Action OnStartButtonClicked;
+        public event Action OnNewGameButtonClicked;
         
-        [SerializeField, HighlightIfNull] private CustomButton _startButton;
+        /// <summary>
+        /// ゲームを続きから始めるボタンを押したときのコールバック
+        /// </summary>
+        public event Action OnLoadGameButtonClicked;
+        
+        /// <summary>
+        /// 設定画面を開くボタンを押したときのコールバック
+        /// </summary>
+        public event Action OnConfigButtonClicked;
+        
+        [Header("ボタンの参照")]
+        [SerializeField, HighlightIfNull] private CustomButton _newGameButton;
+        [SerializeField, HighlightIfNull] private CustomButton _loadGameButton;
+        [SerializeField, HighlightIfNull] private CustomButton _configButton;
+        
+        [Header("色の設定")]
+        [SerializeField] private Color _clickedColor = Color.cyan;
         
         public override UniTask OnAwake()
         {
             // イベント登録
-            _startButton.onClick.SafeAddListener(HandleStartButtonClicked);
+            _newGameButton.onClick.SafeAddListener(HandleNewGameButtonClicked);
+            _loadGameButton.onClick.SafeAddListener(HandleLoadGameButtonClicked);
+            _configButton.onClick.SafeAddListener(HandleConfigButtonClicked);
 
-            if (_startButton != null)
-            {
-                // 確実に有効化しておく
-                _startButton.enabled = true;
-            }
+            // ボタンを確実に有効化する
+            ButtonEnabled(_newGameButton, true);
+            ButtonEnabled(_loadGameButton, true);
+            ButtonEnabled(_configButton, true);
            
             return base.OnAwake();
         }
 
         /// <summary>
-        /// スタートボタンを押したときの処理
+        /// ゲームを最初から始めるボタンを押したときの処理
         /// </summary>
-        private void HandleStartButtonClicked()
+        private void HandleNewGameButtonClicked()
         {
-            if (_startButton != null)
-            {
-                // 二度押しされないようにボタン無効化
-                _startButton.enabled = false;
-                
-                // 押したことが分かりやすいように色を変更
-                _startButton.image.color = Color.cyan;
-            }
-            
-            OnStartButtonClicked?.Invoke();
+            GameStartButtonClicked(_newGameButton);
+            OnNewGameButtonClicked?.Invoke();
         }
 
+        /// <summary>
+        /// ゲームを続きから始めるボタンを押したときの処理
+        /// </summary>
+        private void HandleLoadGameButtonClicked()
+        {
+            GameStartButtonClicked(_loadGameButton);
+            OnLoadGameButtonClicked?.Invoke();
+        }
+
+        /// <summary>
+        /// 設定画面を開くボタンを押したときの処理
+        /// </summary>
+        private void HandleConfigButtonClicked()
+        {
+            // 特に二度押し対策は行っていない
+            OnConfigButtonClicked?.Invoke();
+        }
+        
+        /// <summary>
+        /// nullチェックをしてボタンの有効/無効を切り替える
+        /// </summary>
+        private void ButtonEnabled(CustomButton button, bool isEnabled)
+        {
+            if (button != null)
+            {
+                button.enabled = isEnabled;
+            }
+        }
+
+        /// <summary>
+        /// ゲーム開始ボタンが押されたときの処理（二度押し対策とボタンの色変更）
+        /// </summary>
+        private void GameStartButtonClicked(CustomButton button)
+        {
+            ButtonEnabled(_newGameButton, false);
+            ButtonEnabled(_loadGameButton, false);
+            ButtonEnabled(_configButton, false);
+            
+            // ボタンの色を変更する
+            button.image.color = _clickedColor;
+        }
+        
+        /// <summary>
+        /// Destory
+        /// </summary>
         private void OnDestroy()
         {
-            _startButton.onClick?.SafeRemoveAllListeners();
+            _newGameButton.onClick?.SafeRemoveAllListeners();
+            _loadGameButton.onClick?.SafeRemoveAllListeners();
+            _configButton.onClick?.SafeRemoveAllListeners();
         }
     }
 }
