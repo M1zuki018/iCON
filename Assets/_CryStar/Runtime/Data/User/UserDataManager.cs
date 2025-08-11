c9caa72541fd80420f81643f7f0e77e7ceaf35bd
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-11 23:34:47 +0900

    add:ユーザーデータ生成処理を作成

--- Assets/_CryStar/Runtime/Data/User/UserDataManager.cs
+++ Assets/_CryStar/Runtime/Data/User/UserDataManager.cs
@@ -1,22 +1,41 @@
+using System;
 using CryStar.Core;
 using CryStar.Core.Enums;
-using Cysharp.Threading.Tasks;
+using CryStar.Utility;
+using UnityEngine;
 
 namespace CryStar.Data
 {
     /// <summary>
     /// ユーザーデータ管理クラス
     /// </summary>
-    public class UserDataManager : CustomBehaviour
+    [DefaultExecutionOrder(-998)]
+    public class UserDataManager : MonoBehaviour
     {
+        private static int _userId = -1;
         private UserDataContainer _currentUserData;
+        
         public UserDataContainer CurrentUserData => _currentUserData;
 
-        public override async UniTask OnAwake()
+        private void Awake()
         {
             // グローバルサービスに登録しておく
-            await base.OnAwake();
             ServiceLocator.Register(this, ServiceType.Global);
+
+            // TODO: 仮
+            if (_userId < 0)
+            {
+                // UserIdが0以下 = 未設定の場合、GUIDを元にユーザーIDを生成する
+                var guid = Guid.NewGuid();
+                _userId = Math.Abs(guid.GetHashCode());
+                LogUtility.Info($"UserDataを作成しました ID:{_userId}");
+            }
+
+            if (_currentUserData == null)
+            {
+                // UserDataが存在しない場合、データを作成する
+                CreateUserData(_userId);
+            }
         }
 
         /// <summary>
