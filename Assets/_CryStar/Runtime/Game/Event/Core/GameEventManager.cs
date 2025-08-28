edc603dd1d89df3b1431b9a21c4bcd16ea0c8e5f
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-28 16:59:02 +0900

    feat:イベントマネージャーの、ゲームイベント実行処理の実装を進めた

--- Assets/_CryStar/Runtime/Game/Event/Core/GameEventManager.cs
+++ Assets/_CryStar/Runtime/Game/Event/Core/GameEventManager.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using CryStar.Core;
 using CryStar.Core.Enums;
+using CryStar.Game.Data;
+using CryStar.Game.Enums;
 using CryStar.Game.Events.Initialization;
 using Cysharp.Threading.Tasks;
+using iCON.System;
 
 namespace CryStar.Game.Events
 {
@@ -11,17 +15,40 @@ namespace CryStar.Game.Events
     public class GameEventManager : CustomBehaviour
     {
         /// <summary>
+        /// 各ゲームイベントの列挙型と処理を行うHandlerのインスタンスのkvp
+        /// </summary>
+        private Dictionary<GameEventType, GameEventHandlerBase> _handlers = new Dictionary<GameEventType, GameEventHandlerBase>();
+
+        /// <summary>
+        /// イベントIDとそのIDのイベントデータのkvp
+        /// </summary>
+        private Dictionary<int, GameEventData> _eventData = new Dictionary<int, GameEventData>();
+        
+        /// <summary>
         /// Awake
         /// </summary>
-        public override async UniTask OnAwake()
+        public override async UniTask OnBind()
         {
-            await base.OnAwake();
+            await base.OnBind();
             
             // インゲーム・バトルシーンで利用するためGlobalサービスに登録
             ServiceLocator.Register(this, ServiceType.Global);
             
             // 念のためゲームイベントシステムが初期化されていることを確認する
             GameEventInitializer.Initialize();
+            _handlers = GameEventFactory.CreateAllHandlers(ServiceLocator.GetLocal<InGameManager>());
+        }
+
+        /// <summary>
+        /// イベントIDを元にイベントを実行する
+        /// </summary>
+        public async UniTask ExecuteEvent(int eventID)
+        {
+            var eventData = _eventData[eventID];
+            var eventType = eventData.EventType;
+            var handler = _handlers[eventType];
+            
+            await handler.HandleGameEvent(eventData.Parameter);
         }
     }
 }
