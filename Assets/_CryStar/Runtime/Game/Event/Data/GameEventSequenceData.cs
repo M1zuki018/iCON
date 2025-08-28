192b1eaa5bd2475ddf3df6d2ea4e4ff0096c8f14
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-28 17:47:20 +0900

    feat:開始時・終了時のイベントの実行データをまとめたデータクラス

---
+++ Assets/_CryStar/Runtime/Game/Event/Data/GameEventSequenceData.cs
@@ -0,0 +1,39 @@
+using CryStar.Game.Data;
+
+namespace CryStar.Game.Events
+{
+    /// <summary>
+    /// ゲームイベントの開始時のイベントと終了時のイベントを持った最大のデータ
+    /// </summary>
+    public class GameEventSequenceData
+    {
+        private int _id;
+        private GameEventExecutionData _startEvent;
+        private GameEventExecutionData _endEvent;
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id => _id;
+        
+        /// <summary>
+        /// 開始時のイベント
+        /// </summary>
+        public GameEventExecutionData StartEvent => _startEvent;
+        
+        /// <summary>
+        /// 終了時のイベント
+        /// </summary>
+        public GameEventExecutionData EndEvent => _endEvent;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public GameEventSequenceData(int id, GameEventExecutionData startEvent, GameEventExecutionData endEvent)
+        {
+            _id = id;
+            _startEvent = startEvent;
+            _endEvent = endEvent;
+        }
+    }
+}
