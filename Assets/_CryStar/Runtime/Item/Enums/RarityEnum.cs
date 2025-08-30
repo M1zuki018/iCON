3419b3165373d1ba59dcbb34f3e000c6181dc184
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-08-31 03:52:47 +0900

    feat:レアリティの列挙型

---
+++ Assets/_CryStar/Runtime/Item/Enums/RarityEnum.cs
@@ -0,0 +1,34 @@
+namespace CryStar.Item.Enums
+{
+    /// <summary>
+    /// アイテムのレアリティの列挙型
+    /// </summary>
+    public enum RarityType
+    {
+        /// <summary>
+        /// ノーマル（最低レアリティ）
+        /// </summary>
+        N = 0,
+        
+        /// <summary>
+        /// レア
+        /// </summary>
+        R = 1,
+        
+        /// <summary>
+        /// スーパーレア
+        /// </summary>
+        SR = 2,
+        
+        /// <summary>
+        /// SSR
+        /// </summary>
+        SSR = 3,
+        
+        /// <summary>
+        /// アルティメットレア（最高レアリティ）
+        /// </summary>
+        UR = 4,
+    }
+}
+
