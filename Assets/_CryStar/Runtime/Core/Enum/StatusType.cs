498011a8dd85e54eb16e0c0fc0c0768be87b9a86
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-09-02 12:04:57 +0900

    feat:パラメーターステータスの列挙型

---
+++ Assets/_CryStar/Runtime/Core/Enum/StatusType.cs
@@ -0,0 +1,24 @@
+namespace CryStar.Core.Enums
+{
+    /// <summary>
+    /// パラメーターの種類の列挙型
+    /// </summary>
+    public enum StatusType
+    {
+        Level,
+        Hp,
+        Will,
+        Stamina,
+        Sp,
+        PhysicalAttack,
+        SkillAttack,
+        Intelligence,
+        PhysicalDefense,
+        SkillDefense,
+        Speed,
+        DodgeSpeed,
+        ArmorPenetration,
+        CriticalRate,
+        CriticalDamage
+    }
+}
